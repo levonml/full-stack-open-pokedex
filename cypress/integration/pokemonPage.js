00a48cc5ab2ac9a7edad6e7452633d3374a4d8c8
Levon Stepanyan <levonmll@gmail.com>
2022-04-01 19:55:56 +0300

    fix lint error

--- cypress/integration/pokemonPage.js
+++ cypress/integration/pokemonPage.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 
 describe('Pokemon page', function() {
   it('entering pokemon page', function() {
-	  cy.visit('http://localhost:8080/pokemon/onix')
-	  cy.contains('speed')
-	  cy.contains('weak armor')
+    cy.visit('http://localhost:8080/pokemon/onix')
+    cy.contains('speed')
+    cy.contains('weak armor')
 
   })
 })
