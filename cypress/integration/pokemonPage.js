53d5d7f4150123a852ada876cd83e177c84771d2
Levon Stepanyan <levonmll@gmail.com>
2022-04-01 19:28:11 +0300

    add e2e test

---
+++ cypress/integration/pokemonPage.js
@@ -0,0 +1,9 @@
+
+describe('Pokemon page', function() {
+  it('entering pokemon page', function() {
+	  cy.visit('http://localhost:8080/pokemon/onix')
+	  cy.contains('speed')
+	  cy.contains('weak armor')
+
+  })
+})
