cafd36503b7a1eede70e68a8517af61ce05f9d81
Levon Stepanyan <levonmll@gmail.com>
2022-04-02 00:57:33 +0300

    change port to 8080

--- cypress/integration/pokemonPage.js
+++ cypress/integration/pokemonPage.js
@@ -2,7 +2,7 @@
 
 describe('Pokemon page', function() {
   it('entering pokemon page', function() {
-    cy.visit('http://localhost:5000/pokemon/onix')
+    cy.visit('http://localhost:8080/pokemon/onix')
     cy.contains('speed')
     cy.contains('weak armor')
 
