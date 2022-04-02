20b6b527936686832a96407a3f951948071279fc
Levon Stepanyan <levonmll@gmail.com>
2022-04-02 20:55:16 +0300

    add use(history) middlware

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const history = require('connect-history-api-fallback')
 const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
-
+app.use(history())
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
--- cypress/integration/pokemonPage.js
+++ cypress/integration/pokemonPage.js
@@ -1,10 +1,8 @@
-/* const cy = require('cypress')
-
 describe('Pokemon page', function() {
   it('entering pokemon page', function() {
-    cy.visit('http://localhost:8080/pokemon/onix')
+    cy.visit('http://localhost:5000/pokemon/onix')
     cy.contains('speed')
     cy.contains('weak armor')
 
   })
-}) */
+})
